2e9e8b9020daa1ad7163b3ae3d8ebc8518dc4341
NitrousOX <poljvasdavid@gmail.com>
2024-05-29 23:48:40 +0200

    pocetak

---
+++ NetworkService/NetworkService/NetworkService/Model/ReactorTemp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class ReactorTemp : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _title;
+        private double _value;
+        private ThermoType _type;
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if(_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged(nameof(Title));
+                }
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (value < 250 || value > 350)
+                    throw new ArgumentException("The value of temp must be between 250 and 350");
+
+                if (_value != value)
+                {
+                    _value = value;
+                    OnPropertyChanged(nameof(Value));
+                }
+            }
+        }
+
+        public ThermoType Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                if (_type != value)
+                {
+                    _type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ NetworkService/NetworkService/NetworkService/Model/ThermoType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkService.Model
+{
+    public class ThermoType
+    {
+        private string _name;
+        private string _imgPath;
+
+        public ThermoType(string name, string imgPath)
+        {
+            Name = name;
+            ImgPath = imgPath;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public string ImgPath { get => _imgPath; set => _imgPath = value; }
+    }
+}
